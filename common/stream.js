47facab59ccc06e3e8ce181beb6197a70729b331
Konkor <kapa76@gmail.com>
2017-10-20 03:20:15 +0300

    Add gzip obmin extension Add new streamer

---
+++ common/stream.js
@@ -0,0 +1,330 @@
+/*
+ * Obmin - Simple File Sharing Server
+ *
+ * Copyright (C) 2017 Kostiantyn Korienkov <kapa76@gmail.com>
+ *
+ * This file is part of Obmin File Server.
+ *
+ * Obmin is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Obmin is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+const Lang = imports.lang;
+const GLib = imports.gi.GLib;
+const Gio = imports.gi.Gio;
+const Soup = imports.gi.Soup;
+
+const FileStream = new Lang.Class({
+    Name: 'FileStream',
+    _init: function (server, message, file, finfo, count) {
+        this.BUFFER = 65536;
+        this.server = server;
+        this.msg = message;
+        this.version = message.get_http_version ();
+        this.msg.connect ("wrote-chunk", Lang.bind (this, this.wrote_chunk));
+        this.msg.connect ("finished", ()=>{
+            //debug ("ContentStream " + this.num + " finished");
+            this.done = true;
+            if (this.read_event != 0) {
+                GLib.source_remove (this.read_event);
+                this.read_event = 0;
+            }
+            try{if (this.stream) this.stream.close_async (100, null, null);
+            }catch (err) {
+            error ("Error close stream " + err);
+            }
+            this.b = [];
+            try{this.msg.set_status (200);
+            }catch (err) {
+            error ("Error set status " + err);
+            }
+            try{
+            this.msg.response_body.complete ();
+            }catch (err) {
+            error ("Error complete " + err);
+            }
+        });
+        this.file = file;
+        this.mime = finfo.get_content_type ();
+        this.size = finfo.get_size ();
+        this.range = this.size - 1;
+        this.date = new Date (finfo.get_attribute_uint64 (Gio.FILE_ATTRIBUTE_TIME_MODIFIED)*1000).toUTCString();
+        this.num = count.toString();
+        this.stream = null;
+        this.offset = 0;
+        this.uploaded = 0;
+        this.wrotes = 0;
+        this.reads = 0;
+        this.read_event = 0;
+        this.done = false;
+        this.msg.request_headers.foreach ( (n, v) => {
+            if (n.indexOf ("Range") > -1)
+                if (v.indexOf ("bytes=") > -1) {
+                    let s = v.slice (6).split("-");
+                    if (s[0]) this.offset = parseInt(s[0]);
+                    if (s[1]) this.range = parseInt(s[1]);
+                }
+        });
+        this.range_type = (this.range - this.offset +1) < this.size;
+        //debug ("Request(" + this.num + ") offset " + this.offset + ":" + this.range + " from " + this.size);
+        if (this.offset >= this.size) {
+            this.done = true;
+            this.msg.set_status (200);
+            this.msg.response_body.complete ();
+            this.server.unpause_message (this.msg);
+        }
+        //debug ("Start steamimg");
+        this.file.read_async (GLib.PRIORITY_DEFAULT, null, Lang.bind (this, this.opened));
+    },
+
+    wrote_chunk: function (sender, msg) {
+        this.wrotes++;
+    },
+
+    opened: function (sender, result) {
+        try {
+            this.msg.response_headers.set_encoding (Soup.Encoding.CHUNKED);
+            this.msg.response_headers.set_content_type (this.mime, null);
+            this.msg.response_headers.set_content_length (this.range - this.offset + 1);
+            this.msg.response_headers.append ("Last-Modified", this.date);
+            this.msg.response_headers.append ("Accept-Ranges", "bytes");
+            this.msg.response_headers.append ("Server", "Obmin");
+            this.msg.response_headers.append ("Content-Disposition", "filename=\"" + this.file.get_basename () + "\"");
+            this.msg.response_headers.set_content_range (this.offset,this.range,this.size);
+            this.msg.response_body.set_accumulate (false);
+            this.stream = this.file.read_finish (result);
+            if ((this.version == 1) && this.range_type) this.msg.set_status (206);
+            else this.msg.set_status (200);
+            this.read_more (this.offset > 0);
+        } catch (err) {
+            error (err);
+            this.msg.set_status (500);
+            this.server.unpause_message (this.msg);
+        }
+    },
+
+    read_more: function (seek) {
+        if (this.read_event != 0) {
+            GLib.source_remove (this.read_event);
+            this.read_event = 0;
+        }
+        if (this.done) return;
+        if (seek) this.stream.seek (this.offset, GLib.SeekType.SET, null);
+        if ((this.reads - this.wrotes) > 32) {
+            this.read_event = GLib.timeout_add (0, 250, Lang.bind (this, this.read_more));
+            return;
+        }
+        if ((this.range - this.offset + 1) < this.BUFFER) this.BUFFER = this.range - this.offset + 1;
+        this.stream.read_bytes_async (this.BUFFER, GLib.PRIORITY_DEFAULT, null, Lang.bind (this, this.read_done));
+        this.reads++;
+    },
+
+    read_done: function (sender, result) {
+        try {
+            this.b = this.stream.read_bytes_finish (result);
+            if (this.done) {
+                //debug ("Stream " + this.num + " done");
+                this.b = [];
+                this.msg.response_body.complete ();
+                this.stream.close_async (100, null, null);
+                //counter.upload += this.b.get_size ();
+                return;
+            }
+            if (this.b.get_size() == 0) {
+                //debug (this.b + " " + this.b.get_size());
+                this.done = true;
+                this.msg.response_body.complete ();
+                this.server.unpause_message (this.msg);
+                this.stream.close_async (100, null, null);
+            } else {
+                this.offset += this.b.get_size ();
+                this.msg.response_body.append_buffer (new Soup.Buffer (this.b.get_data()));
+                this.server.unpause_message (this.msg);
+                this.uploaded += this.b.get_size ();
+                if (this.offset < this.range)
+                    this.read_event = GLib.timeout_add (0, 10, Lang.bind (this, this.read_more));
+            }
+        } catch (err) {
+            error ("read_done" + err);
+            if (this.offset == 0) this.msg.set_status (500);
+            else this.msg.response_body.complete ();
+            this.server.unpause_message (this.msg);
+            this.done = true;
+        }
+    }
+});
+
+const PipeStream = new Lang.Class({
+    Name: 'PipeStream',
+    _init: function (server, message, args, name, mime, count) {
+        //debug (args);
+        this.BUFFER = 1048576;
+        this.b = [];
+        this.bsize = 0;
+        this.offset = 0;
+        this.server = server;
+        this.msg = message;
+        this.version = message.get_http_version ();
+        this.msg.connect ("wrote-chunk", Lang.bind (this, this.wrote_chunk));
+        this.msg.connect ("finished", ()=>{
+            //debug ("PipeStream " + this.num + " finished");
+            this.done = true;
+            if (this.read_event != 0) {
+                GLib.source_remove (this.read_event);
+                this.read_event = 0;
+            }
+            try{if (this.stream) this.stream.close_async (100, null, null);
+            }catch (err) {
+            error ("Error close stream " + err);
+            }
+            this.b = [];
+            try{this.msg.set_status (200);
+            }catch (err) {
+            error ("Error set status " + err);
+            }
+            try{
+            this.msg.response_body.complete ();
+            }catch (err) {
+            error ("Error complete " + err);
+            }
+        });
+        this.name = name;
+        this.mime = mime;
+        this.date = new Date().toUTCString();
+        this.num = count.toString();
+        this.stream = null;
+        this.wrotes = 0;
+        this.reads = 0;
+        this.read_event = 0;
+        this.done = false;
+        this.add_headers ();
+        //debug ("Start steamimg pipeline " + this.num);
+        this.stdout_fd = 0;
+        try{
+            let exit, pid, stdin_fd, stderr_fd;
+            [exit, pid, stdin_fd, this.stdout_fd, stderr_fd] = GLib.spawn_async_with_pipes ("/",
+                                            args,
+                                            GLib.get_environ(),
+                                            GLib.SpawnFlags.DO_NOT_REAP_CHILD,
+                                            null);
+            this.stdout = new Gio.UnixInputStream ({fd: this.stdout_fd, close_fd: true});
+            this.stream = Gio.DataInputStream.new (this.stdout);
+            this.stream.buffer_size = this.BUFFER;
+            GLib.close (stdin_fd);
+            GLib.close (stderr_fd);
+            let watch = GLib.child_watch_add (GLib.PRIORITY_DEFAULT, pid, Lang.bind (this, (pid, status, o) => {
+                //debug ("watch handler " + pid + ":" + status + ":" + o);
+                GLib.source_remove (watch);
+            }));
+            this.read_more ();
+        } catch (e) {
+            error (e);
+            this.msg.set_status (500);
+            this.server.unpause_message (this.msg);
+        }
+    },
+
+    wrote_chunk: function (sender, msg) {
+        this.wrotes++;
+    },
+
+    add_headers: function (sender, result) {
+        try {
+            this.msg.response_headers.set_encoding (Soup.Encoding.CHUNKED);
+            this.msg.response_headers.set_content_type (this.mime, null);
+            this.msg.response_headers.append ("Last-Modified", this.date);
+            this.msg.response_headers.append ("Server", "Obmin");
+            this.msg.response_headers.append ("Content-Disposition", "filename=\"" + this.name + "\"");
+            this.msg.response_body.set_accumulate (false);
+            if (this.version == 1) this.msg.set_status (206);
+            else this.msg.set_status (200);
+        } catch (e) {
+            error (e);
+            this.msg.set_status (500);
+            this.server.unpause_message (this.msg);
+        }
+    },
+
+    read_more: function () {
+        if (this.read_event != 0) {
+            GLib.Source.remove (this.read_event);
+            this.read_event = 0;
+        }
+        if (this.done) return;
+        if ((this.reads - this.wrotes) > 32) {
+            this.read_event = GLib.timeout_add (0, 250, Lang.bind (this, this.read_more));
+            return;
+        }
+        this.stream.read_bytes_async (this.BUFFER, GLib.PRIORITY_DEFAULT, null, Lang.bind (this, this.read_done));
+        this.reads++;
+    },
+
+    read_done: function (sender, result) {
+        try {
+            this.b = this.stream.read_bytes_finish (result);
+            if (this.b.get_size() == 0) {
+                //debug ("read_done offset:" + this.offset);
+                this.msg.response_headers.set_content_length (this.offset);
+                if (this.offset == 0) this.msg.set_status (500);
+                else this.msg.set_status (200);
+                this.complete ();
+            } else if (this.done) {
+                //debug ("Stream " + this.num + " done");
+                msg.response_headers.set_content_length (this.offset);
+                if (this.offset == 0) this.msg.set_status (500);
+                else this.msg.set_status (200);
+                this.complete ();
+                return;
+            } else {
+                this.offset += this.b.get_size();
+                this.msg.response_body.append_buffer (new Soup.Buffer (this.b.get_data()));
+                this.server.unpause_message (this.msg);
+                this.read_event = GLib.timeout_add (0, 10, Lang.bind (this, this.read_more));
+            }
+        } catch (err) {
+            error ("read_done " + err);
+            if (this.offset == 0) this.msg.set_status (500);
+            else this.msg.set_status (200);
+            this.complete ();
+        }
+    },
+
+    complete: function () {
+        this.done = true;
+        try {
+            this.msg.response_body.complete ();
+            this.server.unpause_message (this.msg);
+        } catch (e) {
+            error (e.message);
+        }
+        try {
+            GLib.close (this.stdout_fd);
+        } catch (e) {
+            error (e.message);
+        }
+        try {
+            this.stream.close_async (100, null, null);
+        } catch (e) {
+            error (e.message);
+        }
+        this.b = [];
+    }
+});
+
+function debug (text) {
+    print ("streams", text);
+}
+
+function error (text) {
+    printerr ("streams", text);
+}
---
+++ plugins/gzip@konkor/plugin.js
@@ -0,0 +1,161 @@
+/*
+ * Obmin - Simple File Sharing Server
+ *
+ * Copyright (C) 2017 Kostiantyn Korienkov <kapa76@gmail.com>
+ *
+ * This file is part of Obmin File Server.
+ *
+ * Obmin is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Obmin is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+const Lang = imports.lang;
+const GLib = imports.gi.GLib;
+const Gio = imports.gi.Gio;
+const Soup = imports.gi.Soup;
+const GObject = imports.gi.GObject;
+
+const APPDIR = get_appdir ();
+imports.searchPath.unshift (APPDIR);
+const Base = imports.plugins.base;
+const Stream = imports.common.stream;
+
+var LOG_DOMAIN = "tar";
+
+var METADATA = {
+    name: "gzip@konkor",
+    uuid: "bd269ad77d725c4e8fa19ecd59e5dd68",
+    summary: "GZIP File Compressor",
+    tooltip: "Compress files to gzip archive...",
+    schema: "obmin.plugins.konkor.gzip",
+    author: "konkor",
+    url: "https://github.com/konkor/obmin/",
+    version: 1,
+    api: 1,
+    type: Base.PlugType.MENU_ITEM
+};
+
+var LISTS = [
+{query:"tar", title: "Flat Folder With Tar", label: "TAR+"},
+{query:"gz", title: "Compress Folder With GZip", label: "GZIP+"},
+{query:"bz", title: "Compress Folder With BZip2", label: "BZ2+"},
+{query:"xz", title: "Compress Folder With XZ", label: "XZ+"},
+];
+
+const Plugin = new Lang.Class ({
+    Name: 'GzipPlugin',
+    Extends: Base.Plugin,
+
+    _init: function (obmin) {
+        this.parent (obmin, METADATA);
+    },
+
+    menu_item: function (class_name) {
+        let s = "";
+        LISTS.forEach (l => {
+            s += "<a href=\"?plug=" + this.puid + "&format=" + l.query + "\" class=\"" +
+            class_name + "\" onclick=\"toggle()\" title=\"" + l.title + "\">" + l.label + "</a>";
+        });
+        return s;
+    },
+
+    response: function (server, msg, path, query, client, num) {
+        let file, r, recursive = false;
+        if (path == '/') return this.root_handler (server, msg);
+        [file, r] = this.obmin.get_file (path);
+        if (!file) [file, r] = this.get_file (GLib.uri_unescape_string (path, null));
+        if (!file) return false;
+        if (query && query.recursive && query.recursive == 1) recursive = true;
+        if (query && query.format) 
+            return this.get_tar (server, msg, file, r, query.format, recursive, num);
+        return false;
+    },
+
+    get_tar: function (server, msg, dir, rec_attr, ext, recursive, num) {
+        var finfo = dir.query_info ("standart::*", 0, null), path;
+        let mime, archive = dir.get_basename(), args = [];
+        let tar = GLib.find_program_in_path("tar");
+        debug ("tar", tar);
+        if (!tar) return false;
+        args.push (tar);
+        if (finfo.get_is_symlink ())
+            path = Gio.File.new_for_path (finfo.get_symlink_target ());
+        else path = dir;
+        if (!this.obmin.check_backup) args.push ("--exclude-backups");
+        args.push ("-C"); args.push (path.get_parent().get_path ());
+        if (ext == "tar") {
+            args.push ("-cf");
+            archive += ".tar";
+            mime = "application/x-tar";
+        } else if (ext == "gz") {
+            args.push ("-zcf");
+            archive += ".tar.gz";
+            mime = "application/x-compressed-tar";
+        } else if (ext == "bz") {
+            args.push ("-jcf");
+            archive += ".tar.bz2";
+            mime = "application/x-bzib-compressed-tar";
+        } else if (ext == "xz") {
+            args.push ("-Jcf");
+            archive += ".tar.xz";
+            mime = "application/x-xz-compressed-tar";
+        } else return false;
+        args.push ("-");
+        args.push (path.get_basename());
+        //args.push (".");
+        //if (!recursive || !rec_attr) args.push ("--exclude=\'*/*\'");
+        let st = new Stream.PipeStream (server, msg, args, archive, mime, num);
+        this.obmin.ready (-1);
+        msg.connect ("finished", Lang.bind (this, (o)=> {
+            debug ("pipe finished " + st.num);
+            this.obmin.ready (1);
+            this.obmin.upload (st.offset);
+            st = null;
+        }));
+        return true;
+    }
+});
+
+function debug (domain, text) {
+    if (!text) Base.debug (LOG_DOMAIN, domain);
+    else Base.debug (domain, text);
+}
+
+function error (domain, text) {
+    if (!text) Base.error (LOG_DOMAIN, domain);
+    else Base.error (domain, text);
+}
+
+function getCurrentFile () {
+    let stack = (new Error()).stack;
+    let stackLine = stack.split('\n')[1];
+    if (!stackLine)
+        throw new Error ('Could not find current file');
+    let match = new RegExp ('@(.+):\\d+').exec(stackLine);
+    if (!match)
+        throw new Error ('Could not find current file');
+    let path = match[1];
+    let file = Gio.File.new_for_path (path).get_parent().get_parent();
+    return [file.get_path(), file.get_parent().get_path(), file.get_basename()];
+}
+
+function get_appdir () {
+    let s = getCurrentFile ()[1];
+    if (GLib.file_test (s + "/obmin-server", GLib.FileTest.EXISTS)) return s;
+    s = GLib.get_home_dir () + "/.local/share/gnome-shell/extensions/obmin@konkor";
+    if (GLib.file_test (s + "/obmin-server", GLib.FileTest.EXISTS)) return s;
+    s = "/usr/share/gnome-shell/extensions/obmin@konkor";
+    if (GLib.file_test (s + "/obmin-server", GLib.FileTest.EXISTS)) return s;
+    throw "Obmin installation not found...";
+    return s;
+}
